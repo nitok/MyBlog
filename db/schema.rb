7014dd8c60e0d4fee753aa7db7157e13a8dceb1a
EC2 Default User <ec2-user@ip-172-31-25-97.us-east-2.compute.internal>
2019-05-01 12:30:03 +0000

    Add name

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190424081811) do
+ActiveRecord::Schema.define(version: 20190501122814) do
 
   create_table "messages", force: :cascade do |t|
     t.string   "title"
     t.string   "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "name"
+  end
+
+  create_table "sakes", force: :cascade do |t|
+    t.string   "prefecture"
+    t.string   "brand"
+    t.string   "item"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
